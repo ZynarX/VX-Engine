1cf036a1804ae8145ff20dd374c5fa979b49ac1e
ZynarX <coldevyt@gmail.com>
2025-07-11 17:00:41 +0400

    Update Game.hpp

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
-#include <map>
+#include <random>
 #include <SDL.h>
 #include "Player.hpp"
 #include "Entity.hpp"
 #include "Item.hpp"
 #include "Trap.hpp"
 #include "Struct.hpp"
+#include "EntityComponentSystem.h"
+#pragma once
 
 class Game
 {
@@ -17,13 +19,15 @@ private:
 
 	std::string filename;
 	std::vector<SDL_Rect> MAP;
-	std::vector<Uint32> background_colors;
+	Color background_colors;
 	
 	bool is_running;
 	bool combat;
 
 	bool COMBAT_ENABLED;
 	bool COLLISION_ENABLED;
+
+	int combat_entity_index;
 
 	float FIXED_TIME_STEP;
 	float FRAME_DELAY;
@@ -37,10 +41,14 @@ private:
 	void HandleInput(SDL_Event& event);
 	void render(SDL_Renderer* renderer);
 	void HandleMovement(float deltaTime);
-	bool CheckCollision(SDL_Rect a, SDL_Rect b);
 
-	void HandleCombat(Entity& entity, size_t entity_index);
+	bool CheckCollision(SDL_Rect a, SDL_Rect b);
+	bool CheckCombat();
+
+	void HandleCombat(Entity* entity, size_t entity_index);
+
+	void Clear();
 public:
-	Game(std::string filepath, Player& player, std::vector<Entity> Entities, std::vector<Item> Items, std::vector<Trap> Traps, std::vector<Uint32> backgroundColors, int width, int height, bool combat, bool collision);
+	Game(std::string filepath, Player& player, std::vector<Entity> Entities, std::vector<Item> Items, std::vector<Trap> Traps, Color backgroundColors, int width, int height, bool combat, bool collision);
 	bool run();
 };

e79cb473887888ab5eabb86a473bb84cef98a079
ZynarX <coldevyt@gmail.com>
2025-07-11 16:59:06 +0400

    Update Item.cpp

--- src/Item.cpp
+++ src/Item.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include "Item.hpp"
 
-Item::Item(std::string name, int x_position, int y_position, std::vector<Uint32> rgb)
+Item::Item(std::string name, int x_position, int y_position, int buff, Color rgb)
 {
 	this->Name = name;
-	this->XPos = x_position;
-	this->YPos = y_position;
-
-	for (Uint32& item : rgb)
-	{
-		this->RGB.push_back(item);
-	}
+	this->position.X = x_position;
+	this->position.Y = y_position;
+	this->buff = buff;
+	
+	this->color.Red = rgb.Red;
+	this->color.Green = rgb.Green;
+	this->color.Blue = rgb.Blue;
 }
 
 std::string Item::get_name()
 {
 	return this->Name;
 }
-int Item::get_xposition()
+Position Item::get_position()
 {
-	return this->XPos;
+	return this->position;
 }
-int Item::get_yposition()
+int Item::get_buff()
 {
-	return this->YPos;
+	return this->buff;
 }
-std::vector<Uint32> Item::get_color()
+Color Item::get_color()
 {
-	return this->RGB;
+	return this->color;
 }

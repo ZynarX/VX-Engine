7444c193c79f7b73cfd83c16a424040b1309d0f6
ZynarX <coldevyt@gmail.com>
2025-07-11 16:58:27 +0400

    Update Player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include "Player.hpp"
 
-Player::Player(std::string Name, int x_position, int y_position, int health, std::vector<Uint32> rgb, int damage, float speed)
+Player::Player(std::string Name, int x_position, int y_position, int health, Color rgb, int damage, float speed)
 {
 	this->move_up = false;
 	this->move_down = false;
 	this->move_right = false;
 	this->move_left = false;
 	this->name = Name;
-	this->XPos = x_position;
-	this->YPos = y_position;
-	this->Width = 100;
-	this->Height = 100;
+	this->transform.Position.X = x_position;
+	this->transform.Position.Y = y_position;
+	this->transform.Size.X = 100;
+	this->transform.Size.Y = 100;
 	this->Health = health;
 	
-	for (Uint32 &item : rgb)
-	{
-		this->RGB.push_back(item);
-	}
+	this->color.Red = rgb.Red;
+	this->color.Green = rgb.Green;
+	this->color.Blue = rgb.Blue;
 
 	this->Damage = damage;
 	this->Speed = speed;
@@ -46,42 +45,44 @@ void Player::pickup(Item item)
 	this->Inventory.push_back(item);
 	this->Items[item.get_name()] = this->Items[item.get_name()] + 1;
 
+	this->Damage += item.get_buff();
 
-	std::cout << "---- Current Player Inventory ----\n";
+
+	std::cout << "--------------------------- Current Inventory ---------------------------\n";
 
 	for (auto& pair : this->get_items())
 	{
 		std::cout << pair.first << " x" << pair.second << std::endl;
 	}
 
-	std::cout << "----------------------------------\n";
+	std::cout << "-------------------------------------------------------------------------\n";
 
 }
 void Player::move(int x_steps, int y_steps)
 {
-	this->XPos += x_steps;
-	this->YPos += y_steps;
+	this->transform.Position.X += x_steps;
+	this->transform.Position.Y += y_steps;
+}
+
+void Player::stop_movement()
+{
+	this->move_up = false;
+	this->move_down = false;
+	this->move_right = false;
+	this->move_left = false;
 }
 
 std::string Player::get_name()
 {
 	return this->name;
 }
-int Player::get_xposition()
+Position Player::get_position()
 {
-	return this->XPos;
+	return this->transform.Position;
 }
-int Player::get_yposition()
+Size Player::get_size()
 {
-	return this->YPos;
-}
-int Player::get_width()
-{
-	return this->Width;
-}
-int Player::get_height()
-{
-	return this->Height;
+	return this->transform.Size;
 }
 int Player::get_health()
 {
@@ -95,9 +96,9 @@ float Player::get_speed()
 {
 	return this->Speed;
 }
-std::vector<Uint32> Player::get_color()
+Color Player::get_color()
 {
-	return this->RGB;
+	return this->color;
 }
 std::vector<Item> Player::get_inventory()
 {

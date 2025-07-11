edef5bf44d59b29a4a2ddd00c112ea06ab61d0db
ZynarX <coldevyt@gmail.com>
2025-07-11 17:02:33 +0400

    Update Struct.hpp

--- include/Struct.hpp
+++ include/Struct.hpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <SDL.h>
+#include "EntityComponentSystem.h"
 #pragma once
 
 struct Tile {
 	int Type;
-	int X;
-	int Y;
-	Uint32 Red;
-	Uint32 Green;
-	Uint32 Blue;
-	Uint32 Alpha;
+	Position position;
+	Color color;
 };

d7fd1e5e60fd1662cfd0d26973a8b5cccb03ed41
ZynarX <coldevyt@gmail.com>
2025-07-11 16:58:46 +0400

    Update Entity.cpp

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include "Entity.hpp"
 
-Entity::Entity(std::string Name, int x_position, int y_position, int health, std::vector<Uint32> rgb, int damage, float speed)
+Entity::Entity(std::string Name, int x_position, int y_position, int health, Color rgb, Hostility hostility, float speed)
 {
 	this->move_up = false;
 	this->move_down = false;
 	this->move_right = false;
 	this->move_left = false;
 	this->name = Name;
-	this->XPos = x_position;
-	this->YPos = y_position;
+	this->position.X = x_position;
+	this->position.Y = y_position;
+	this->color.Red = rgb.Red;
+	this->color.Green = rgb.Green;
+	this->color.Blue = rgb.Blue;
 	this->Health = health;
 
-	for (Uint32& item : rgb)
-	{
-		this->RGB.push_back(item);
-	}
-
-	this->Damage = damage;
+	this->hostility.mode = hostility.mode;
+	this->hostility.amount = hostility.amount;
 	this->Speed = speed;
 }
 
@@ -41,35 +40,43 @@ void Entity::take_damage(int amount)
 }
 void Entity::move(int x_steps, int y_steps)
 {
-	this->XPos += x_steps;
-	this->YPos += y_steps;
+	this->position.X += x_steps;
+	this->position.Y += y_steps;
+}
+
+void Entity::stop_movement()
+{
+	this->move_up = false;
+	this->move_down = false;
+	this->move_right = false;
+	this->move_left = false;
 }
 
 std::string Entity::get_name()
 {
 	return this->name;
 }
-int Entity::get_xposition()
+Position Entity::get_position()
 {
-	return this->XPos;
-}
-int Entity::get_yposition()
-{
-	return this->YPos;
+	return this->position;
 }
 int Entity::get_health()
 {
 	return this->Health;
 }
+int Entity::get_mode()
+{
+	return this->hostility.mode;
+}
 int Entity::get_damage()
 {
-	return this->Damage;
+	return this->hostility.amount;
 }
 float Entity::get_speed()
 {
 	return this->Speed;
 }
-std::vector<Uint32> Entity::get_color()
+Color Entity::get_color()
 {
-	return this->RGB;
+	return this->color;
 }

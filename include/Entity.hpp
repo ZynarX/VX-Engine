0e1d1e805fc8e71b3a7ec7f8d3ff51a30d77456c
ZynarX <coldevyt@gmail.com>
2025-07-11 17:01:29 +0400

    Update Entity.hpp

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -2,33 +2,35 @@
 #include <vector>
 #include <string>
 #include <SDL.h>
+#include "EntityComponentSystem.h"
 #pragma once
 
 class Entity {
 private:
 	std::string name;
-	int XPos;
-	int YPos;
+	Position position;
+	Hostility hostility;
+	Color color;
 	int Health;
-	int Damage;
 	float Speed;
-	std::vector<Uint32> RGB;
 public:
 	bool move_up;
 	bool move_down;
 	bool move_right;
 	bool move_left;
 
-	Entity(std::string Name, int x_position, int y_position, int health, std::vector<Uint32> rgb, int damage, float speed);
+	Entity(std::string Name, int x_position, int y_position, int health, Color rgb, Hostility hostility, float speed);
 
 	void take_damage(int amount);
 	void move(int x_steps, int y_steps);
 
+	void stop_movement();
+
 	std::string get_name();
-	int get_xposition();
-	int get_yposition();
+	Position get_position();
 	int get_health();
+	int get_mode();
 	int get_damage();
 	float get_speed();
-	std::vector<Uint32> get_color();
+	Color get_color();
 };

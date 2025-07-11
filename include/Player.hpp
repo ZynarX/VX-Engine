123ea071c0971e69fc4a729336c1168cc821a62b
ZynarX <coldevyt@gmail.com>
2025-07-11 17:01:05 +0400

    Update Player.hpp

--- include/Player.hpp
+++ include/Player.hpp
@@ -5,17 +5,15 @@
 #include <SDL.h>
 #include "Item.hpp"
 #include "Entity.hpp"
+#include "EntityComponentSystem.h"
 #pragma once
 
 class Player {
 private:
 	std::string name;
-	int XPos;
-	int YPos;
-	int Width;
-	int Height;
+	Transform transform;
 	int Health;
-	std::vector<Uint32> RGB;
+	Color color;
 	int Damage;
 	float Speed;
 	std::vector<Item> Inventory;
@@ -26,21 +24,21 @@ public:
 	bool move_right;
 	bool move_left;
 
-	Player(std::string name, int x_position, int y_position, int health, std::vector<Uint32> rgb, int damage, float speed);
+	Player(std::string name, int x_position, int y_position, int health, Color rgb, int damage, float speed);
 	
 	void take_damage(int amount);
 	void pickup(Item item);
 	void move(int x_steps, int y_steps);
 
+	void stop_movement();
+
 	std::string get_name();
-	int get_xposition();
-	int get_yposition();
-	int get_width();
-	int get_height();
+	Position get_position();
+	Size get_size();
 	int get_health();
 	int get_damage();
 	float get_speed();
-	std::vector<Uint32> get_color();
+	Color get_color();
 	std::vector<Item> get_inventory();
 	std::map<std::string, int> get_items();
 };
